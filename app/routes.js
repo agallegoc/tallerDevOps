cd8d8e855253ad3919a63d75118e093777877f6e
German Martinez <germaaan@gmail.com>
2017-03-31 00:35:41 +0200

    Añade archivo de rutas

---
+++ app/routes.js
@@ -0,0 +1,35 @@
+"use strict";
+
+module.exports = function(app) {
+    app.use(function(req, res, next) {
+        if (typeof(req.session.todolist) == 'undefined') {
+            req.session.todolist = [];
+        }
+        next();
+    });
+
+    app.get('/todo', function(req, res) {
+        res.render('todo.ejs', {
+            todolist: req.session.todolist
+        });
+    });
+
+    app.post('/todo/add/', urlencodedParser, function(req, res) {
+        if (req.body.newtodo != '') {
+            req.session.todolist.push(req.body.newtodo);
+        }
+        res.redirect('/todo');
+    });
+
+    app.get('/todo/delete/:id', function(req, res) {
+        if (req.params.id != '') {
+            req.session.todolist.splice(req.params.id, 1);
+        }
+        res.redirect('/todo');
+    });
+
+    app.use(function(req, res, next) {
+        res.redirect('/todo');
+    });
+
+};
--- app/serverConfig.js
+++ app/serverConfig.js
@@ -1,31 +1,23 @@
 "use strict";
 
 var favicon = require('serve-favicon');
-var expressStatic = require('express').static;
 var ejs = require('ejs');
-var expressLayouts = require('express-ejs-layouts');
 var bodyParser = require('body-parser');
+var session = require('cookie-session');
 var morgan = require('morgan');
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 module.exports = function(app) {
 	app.set('port', process.env.PORT || 3000);
 	app.set('ip', process.env.IP || "127.0.0.1");
-	app.set('env', process.env.ENV);
-
-	app.engine('ejs', function(filePath, options, callback) {
-		ejs.__express(filePath, options, function(err, html) {
-			if (err) return callback(err);
-			callback(null, htmlMinify(html, minifyConfig));
-		});
-	});
 
 	app.set('views', 'views');
-	app.set('layout', 'layouts/default');
-	app.use(expressLayouts);
 	app.set('view engine', 'ejs');
-	app.use(compress());
+
+  app.use(session({secret: 'miclavesecreta'}))
+
 	app.use(bodyParser.json());
-	if (app.get('env') === "prod") app.enable('view cache');
 
 	app.use(favicon('./public/favicon/favicon.ico'));
 	app.use(morgan('dev'));
